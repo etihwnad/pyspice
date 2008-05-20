691bfb89e265dd837353c0fb39dc1d31372116db
Dan White <dan@whiteaudio.com>
2008-05-20 23:13:17 +0000

    foo
    --HG--
    extra : convert_revision : svn%3A008b89a8-8315-0410-8f62-d1f31a6630f5/%4031

--- parser/parser.py
+++ parser/parser.py
@@ -1,13 +1,36 @@
 
 from pyparsing import *
 
-title = StringStart() + SkipTo(LineEnd())
-comment = StringStart() + Literal('*') + SkipTo(LineEnd())
-element = StringStart() + SkipTo(LineEnd())
-control = LineStart() + Literal('.') + SkipTo(LineEnd())
-card = comment | element 
-deck = title.setName('title') + OneOrMore(card)
-deck = OneOrMore(card)
+title = Group(LineStart() + SkipTo(LineEnd()))
+end = StringEnd()
+#title = StringStart() + SkipTo(LineEnd())
+emptyLine = LineStart() + SkipTo(LineEnd())
+comment = Literal('*') + SkipTo(LineEnd())
+elementStart = Word(alphanums) + SkipTo(LineEnd())
+elementContinue = Literal('+') + SkipTo(LineEnd())
+control = Literal('.') + SkipTo(LineEnd())
 
-for line in open('hspiceFinal'):
-    print card.parseString(line)
+capacitor = (LineStart() + CaselessLiteral('c') + Word(alphanums) + Word(alphanums) + Word('alphanums') + Word(alphanums + '.') + ZeroOrMore(Word(alphanums + '.'))).setResultsName('capacitor')
+
+element = capacitor ^ (elementStart + ZeroOrMore(elementContinue)).setResultsName('element')
+
+card = Group(comment.setResultsName('comment') \
+             | element \
+             | emptyLine.setResultsName('empty') )
+
+deck = title + ZeroOrMore(card) + end
+
+infile = open('netlist').read()
+
+result = deck.parseString(infile)
+
+print result.dump()
+
+for r in result:
+    print r.dump()
+#print result
+
+#result = deck.parseFile(infile)
+
+
+
